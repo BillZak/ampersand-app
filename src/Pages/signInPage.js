1056b8766b1664bcd9232c5d25c920cf1f36eef2
Bill Zack <bzak954@gmail.com>
2021-11-30 15:16:50 +0000

    add navigation prop to function

--- src/Pages/signInPage.js
+++ src/Pages/signInPage.js
@@ -9,16 +9,16 @@ import {
 } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
-export default function SignInPage() {
+export default function SignInPage({ navigation }) {
   return (
     <View style={styles.container}>
       <View style={styles.firstview}>
-        <MaterialIcons
+        <TouchableOpacity
           style={{ position: "absolute", left: 20 }}
-          name="keyboard-backspace"
-          size={30}
-          color="white"
-        />
+          onPress={() => navigation.goBack()}
+        >
+          <MaterialIcons name="keyboard-backspace" size={30} color="white" />
+        </TouchableOpacity>
         <Text style={{ color: "#fff", fontSize: 20, letterSpacing: 2 }}>
           Sign In
         </Text>
@@ -26,7 +26,7 @@ export default function SignInPage() {
       <View style={styles.secondview}>
         <Image
           style={{ height: "100%", width: "100%" }}
-          source={require("./assets/unsplash2.jpg")}
+          source={require("../../assets/unsplash2.jpg")}
         />
       </View>
       <View style={styles.thirdview}>
