db3de6f2c9a709f98dcf7f76ac1caee1da576532
Bill Zack <bzak954@gmail.com>
2021-12-02 16:07:11 +0000

    add pages

--- src/Components/navigator.js
+++ src/Components/navigator.js
@@ -6,6 +6,9 @@ import RegistrationPage from "../Pages/registrationPage";
 import SecondPage from "../Pages/secondPage";
 import SignInPage from "../Pages/signInPage";
 import WelcomePage from "../Pages/WelcomePage";
+import MyProfile from "../Pages/myProfilePage";
+import MemberProfile from "../Pages/memberProfile";
+import ScanPage from "../Pages/scanPage";
 
 const Stack = createStackNavigator();
 
@@ -22,6 +25,9 @@ export default function Navigator() {
         <Stack.Screen name="SecondPage" component={SecondPage} />
         <Stack.Screen name="Registration" component={RegistrationPage} />
         <Stack.Screen name="SignIn" component={SignInPage} />
+        <Stack.Screen name="MyProfile" component={MyProfile} />
+        <Stack.Screen name="MemberProfile" component={MemberProfile} />
+        <Stack.Screen name="Scan" component={ScanPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
