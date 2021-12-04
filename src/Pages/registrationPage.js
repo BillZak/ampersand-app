706cabcd1945faa911a00a45c55c91bfcd6772d8
Bill Zack <bzak954@gmail.com>
2021-12-04 15:59:32 +0000

    add expo image picker

--- src/Pages/registrationPage.js
+++ src/Pages/registrationPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Image,
   StyleSheet,
@@ -7,9 +7,38 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 import { MaterialIcons } from "@expo/vector-icons";
 
 export default function RegistrationPage({ navigation }) {
+  const [image, setImage] = useState(null);
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== "web") {
+        const { status } =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== "granted") {
+          alert("Sorry, we need camera roll permissions to make this work!");
+        }
+      }
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.firstview}>
@@ -25,10 +54,35 @@ export default function RegistrationPage({ navigation }) {
       </View>
 
       <View style={styles.secondview}>
-        <Image
-          style={{ height: "100%", width: "100%" }}
-          source={require("../../assets/1041797.jpg")}
-        />
+        <TouchableOpacity onPress={() => pickImage()} style={{ flex: 1 }}>
+          {image ? (
+            <View style={{ flex: 1 }}>
+              <Image
+                source={{ uri: image }}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  resizeMode: "cover",
+                  position: "absolute",
+                }}
+              />
+              <Text style={styles.touchable}>EDIT PPRFLIE PHOTO</Text>
+            </View>
+          ) : (
+            <View>
+              <Image
+                source={require("../../assets/1041797.jpg")}
+                style={{
+                  width: "100%",
+                  height: 200,
+                  resizeMode: "cover",
+                  position: "absolute",
+                }}
+              />
+              <Text style={styles.photo}>ADD PPRFLIE PHOTO</Text>
+            </View>
+          )}
+        </TouchableOpacity>
       </View>
       <View style={styles.thirdview}>
         <Text style={styles.inputlabel}>Full Name</Text>
@@ -100,5 +154,14 @@ const styles = StyleSheet.create({
   inputlabel: {
     fontSize: 15,
     top: 20,
+  },
+  touchable: {
+    textAlign: "center",
+    marginTop: "20%",
+    marginHorizontal: 40,
+    borderWidth: 2,
+    padding: 10,
+    color: "white",
+    borderColor: "white",
   },
 });
